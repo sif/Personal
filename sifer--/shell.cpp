2c25ea575ff6acb03cfa577025eec525e1ee3303
Sifer Aseph <sifer.aseph@gmail.com>
2016-01-31 09:27:49 -0500

    shelly

--- sifer--/shell.cpp
+++ sifer--/shell.cpp
@@ -6,11 +6,12 @@
 
 #include <iostream>
 
-// 
+//
 
 int main(int argc, const char * argv[]) {
 
     // A loop here. Lifetime of shell starts here.
+    // The idea is to parse. Then...
     
     return 0;
 }
