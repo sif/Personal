8fe18150a2cf44ed2c6367c9f01b35f50ea5815b
Sifer Aseph <sifer.aseph@gmail.com>
2016-05-28 07:57:46 -0400

    I'm human

---
+++ sifer--/trivial 1.cpp
@@ -0,0 +1,16 @@
+//  Sifer Aseph
+//  https://github.com/ryra
+//  More Trivial on Xcode
+
+#include <iostream>
+
+int main() {
+    
+    
+    return 0;
+}
+
+// Apparently this kind of commenting does not exist in C89. Had to fix that on all of my old code.
+// cc1 is GCC's C compiler.
+// Use: gcc -Wall -Wextra -ansi -pedantic stuff.c
+// Fun fact: GCC used to be GNU C Compiler. It's now GNU Compiler Collection. It was created shortly after I was born.
