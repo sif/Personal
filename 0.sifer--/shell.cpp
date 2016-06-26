59688f3689e99f25b1b76b0067d7eb0e0a76abba
Sifer Aseph <sifer.aseph@gmail.com>
2016-06-26 16:10:37 -0400

    shelly

--- 0.sifer--/shell.cpp
+++ 0.sifer--/shell.cpp
@@ -1,18 +1,55 @@
 //
 //  Shell
-//  Ryra
-//  Because I can. Doing this to stem my boredom in class or whatevs.
+//  Sifer Aseph
 //
 
 #include <iostream>
+#include <unistd.h>
+#include <string>
+#include <sstream>
 
-//
+std::string readLine() {
+    std::string userInput;
+    
+    while (true) {
+        std::cin >> userInput;
+    }
+    
+    return userInput;
+}
+
+void start() {
+    int status = 0;
+    std::string line;
+    char **args;
+    
+    line = readLine();
+    
+    while (status) {
+        std::cout << "Sifer's Shell >> ";
+    }
+}
+
+int initializer() {
+    pid_t pid = fork();
+    int status = 0;
+    
+    if (pid == -1) {
+        std::perror("Fork failed.");
+        exit(EXIT_FAILURE);
+    } else if (pid >= 0) {
+        std::cout << "Fork success: child process.\n";
+        _exit(EXIT_SUCCESS);
+    } else {
+        (void)waitpid(pid, &status, 0);
+    }
+    return EXIT_SUCCESS;
+}
 
 int main(int argc, const char * argv[]) {
-    
-    
-    // A loop here. Lifetime of shell starts here.
-    // The idea is to parse. Then...
+    start();
     
     return 0;
 }
+
+//system (" ");
