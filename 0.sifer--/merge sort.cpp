4e59612a5b3f10bc6b6f96a82a3640fdf034660a
Sifer Aseph <sifer.aseph@gmail.com>
2016-11-17 18:26:09 -0500

    Book

--- 0.sifer--/merge sort.cpp
+++ 0.sifer--/merge sort.cpp
@@ -5,6 +5,8 @@
 
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <time.h>
 
 std::vector<int> mergeSort(std::vector<int> &theList) {
     
@@ -14,16 +16,12 @@ std::vector<int> mergeSort(std::vector<int> &theList) {
 
 int main() {
     std::vector<int> tastyPie;
-    tastyPie.push_back(59);
-    tastyPie.push_back(26);
-    tastyPie.push_back(41);
-    tastyPie.push_back(58);
-    tastyPie.push_back(0);
-    tastyPie.push_back(3);
-    tastyPie.push_back(53);
-    tastyPie.push_back(97);
-    tastyPie.push_back(93);
-    tastyPie.push_back(1);
+    srand(std::time(nullptr));
+    int aNumber = 0;
+    for (int tenNumbers = 0; tenNumbers < 10; ++tenNumbers) {
+        aNumber = rand() & 1000;
+        tastyPie.push_back(aNumber);
+    }
     
     for (int i = 0; i < tastyPie.size(); ++i) {
         std::cout << tastyPie[i] << " ";
