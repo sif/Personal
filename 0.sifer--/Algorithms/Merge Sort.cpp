e30b6642a52f095edb1754587e750fba09beb5ce
Sifer Aseph <sifer.aseph@gmail.com>
2016-11-22 00:46:37 -0500

    Merge Sort

--- 0.sifer--/Algorithms/Merge Sort.cpp
+++ 0.sifer--/Algorithms/Merge Sort.cpp
@@ -25,36 +25,52 @@ void mergeSort(int *theList, int low, int high) {
     }
 }
 
-void merge(int *theList, int low, int mid, int high) {
-    int lowIndex = low, midIndex = mid + 1, highIndex = low, newList[1000];
+void merge(int *theList, int low, int mid, int high) { // p, q, r in some literature
+    int lowIndex = 0, midIndex = 0, highIndex = low, newList[1000]; // i, j, k in some literature
     
-    while (lowIndex <= mid && midIndex <= high) {
-        if (theList[lowIndex] < theList[midIndex]) {
-            newList[highIndex] = theList[lowIndex];
-            lowIndex++;
-            highIndex++;
-        } else {
-            newList[highIndex] = theList[midIndex];
-            midIndex++;
-            highIndex++;
-        }
-        
-        while (lowIndex <= mid) {
-            newList[highIndex] = theList[lowIndex];
-            lowIndex++;
-            highIndex++;
-        }
-        
-        while (midIndex <= high) {
-            newList[highIndex] = theList[midIndex];
-            midIndex++;
-            highIndex++;
-        }
-        
-        for (lowIndex = low; lowIndex < highIndex; ++lowIndex) {
-            theList[lowIndex] = newList[lowIndex];
-        }
+    int *leftList = nullptr;
+    int *rightList = nullptr;
+    
+    int n1 = mid - low + 1; // CLRS
+    int n2 = high - mid; // CLRS
+    
+    leftList = new int[n1];
+    rightList = new int[n2];
+    
+    for (int step = 0; step < n1; ++step) {
+        leftList[step] = theList[low + step];
     }
+    
+    for (int step = 0; step < n2; ++step) {
+        rightList[step] = theList[mid + step + 1];
+    }
+    
+    while (lowIndex < n1 && midIndex < n2) {
+        if (leftList[lowIndex] < rightList[midIndex]) {
+            theList[highIndex] = leftList[lowIndex];
+            lowIndex++;
+        } else {
+            theList[highIndex] = rightList[midIndex];
+            midIndex++;
+        }
+        
+        highIndex++;
+    }
+    
+    while (lowIndex < n1) {
+        theList[highIndex] = leftList[lowIndex];
+        lowIndex++;
+        highIndex++;
+    }
+    
+    while (midIndex < n2) {
+        theList[highIndex] = rightList[midIndex];
+        midIndex++;
+        highIndex++;
+    }
+    
+    delete [] leftList;
+    delete [] rightList;
 }
 
 int main() {
@@ -83,3 +99,4 @@ int main() {
 }
 
 // dealing with some off by one issues here :(
+// I should be using either smart pointers or vectors :(
