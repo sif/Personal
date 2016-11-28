32bdd908566e8a733e29845f090d23235693f078
Sifer Aseph <sifer.aseph@nyu.edu>
2016-11-28 15:04:52 -0500

    Eh, useless

---
+++ 0.sifer--/Out of Date/gets.cpp
@@ -0,0 +1,12 @@
+// Sifer Aseph
+
+#include <iostream>
+
+int main() {
+    char input[512];
+    gets(input);
+
+    // Cool: "warning: this program uses gets(), which is unsafe."
+
+    return 0;
+}
