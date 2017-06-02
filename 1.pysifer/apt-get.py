5d4cea9001e6a12057d858d8fad31b2847485b13
Sifer Aseph <sifer.aseph@nyu.edu>
2017-06-02 04:15:43 -0400

    Shepard

---
+++ 1.pysifer/apt-get.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# Sifer Aseph
+# apt-get
+
+import os
+
+def main():
+    os.system( 'sudo apt-get update && sudo apt-get dist-upgrade' )
+
+if __name__ == "__main__":
+    main()
---
+++ 1.pysifer/who.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# Sifer Aseph
+# who
+
+import os
+
+def main():
+    os.system( 'w' )
+
+if __name__ == "__main__":
+    main()
