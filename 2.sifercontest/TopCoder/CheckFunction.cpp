a7e679d168075777c379a84e7be6567be800df29
Sifer Aseph <sifer.aseph@nyu.edu>
2017-06-03 06:55:31 -0400

    Maeryll

---
+++ 2.sifercontest/TopCoder/CheckFunction.cpp
@@ -0,0 +1,64 @@
+/*
+ Sifer Aseph
+ TopCoder: CheckFunction
+ https://community.topcoder.com/stat?c=problem_statement&pm=4788
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <typeinfo>
+
+// -1 returned means something is wrong
+
+class CheckFunction {
+public:
+    int convertMe(int digit) {
+        if (digit == 0) {
+            return 6;
+        } else if (digit == 1) {
+            return 2;
+        } else if (digit == 2) {
+            return 5;
+        } else if (digit == 3) {
+            return 5;
+        } else if (digit == 4) {
+            return 4;
+        } else if (digit == 5) {
+            return 5;
+        } else if (digit == 6) {
+            return 6;
+        } else if (digit == 7) {
+            return 3;
+        } else if (digit == 8) {
+            return 7;
+        } else if (digit == 9) {
+            return 6;
+        }
+        
+        return -1;
+    }
+    
+    int newFunction(std::string code) {
+        std::vector<int> addMe;
+        for (int i = 0; i < code.length(); ++i) {
+            addMe.push_back(convertMe((code[i] - '0')));
+        }
+        
+        int sumMe = 0;
+        
+        for (int j = 0; j < addMe.size(); ++j) {
+            sumMe += addMe[j];
+        }
+        
+        return sumMe;
+    }
+};
+
+int main() {
+    CheckFunction test;
+    std::cout << test.newFunction("13579") << std::endl; // should return 21
+    std::cout << test.newFunction("02468") << std::endl; // 28
+    
+    return 0;
+}
