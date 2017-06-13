ba3682769a0b4444c74fba23d8c3373a4c1f42a6
Sifer Aseph <sifer.aseph@nyu.edu>
2017-06-13 11:54:53 -0400

    Juliana
    Joanna

---
+++ 2.sifercontest/TopCoder/TheBeauty.cpp
@@ -0,0 +1,54 @@
+/*
+ Sifer Aseph
+ TopCoder: TheBeauty
+ https://community.topcoder.com/stat?c=problem_statement&pm=10233
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+#include <map>
+
+class TheBeauty {
+public:
+    int find (int n) {
+        if (n < 10) {
+            return 1;
+        }
+        
+        digits.clear();
+        
+        if (n >= 10) {
+            while (n != 0) {
+                int digit = n % 10;
+                n /= 10;
+                
+                if (std::find(digits.begin(), digits.end(), digit) != digits.end()) {
+                    
+                } else {
+                    digits.push_back(digit);
+                }
+                
+            }
+        }
+        
+        return digits.size();
+    }
+    
+private:
+    std::vector<int> digits;
+};
+
+int main() {
+    TheBeauty test;
+    
+    std::cout << test.find(7) << std::endl; // should return 1
+    std::cout << test.find(100) << std::endl; // 2
+    std::cout << test.find(123456789) << std::endl; // 9
+    std::cout << test.find(1000000000) << std::endl; // 2
+    
+    return 0;
+}
